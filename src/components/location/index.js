851427b22c6afd335b098b815b46742321b2b04f
aismai <abai.ismailov@gmail.com>
2018-09-06 13:39:59 +0600

    add Footer component

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -4,6 +4,7 @@ const Location = () => {
   return (
     <div className="location_wrapper">
       <iframe
+        title="event location"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14845.17506847549!2d-122.45924157852193!3d37.737235501869876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x242c7cdecc3e5db0!2sCity+College+of+San+Francisco+Bookstore!5e0!3m2!1sen!2skg!4v1536217468779"
         width="100%"
         height="500px"
